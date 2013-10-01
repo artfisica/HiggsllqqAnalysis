dc25246808cc97d8a74cd2e95cb47730e7fc7574
Arturo Rodolfo Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2013-10-01 15:22:52 +0000

    Code of Production 13: weight separation and division of cutflow for 0-1-2 btagged bins, bug solved in 1-2 bjets bins

---
+++ tags/HiggsllqqAnalysis-00-00-13/trunk/HiggsllqqAnalysis/CommonTools.h
@@ -0,0 +1,43 @@
+#ifndef __CommonTools_h__
+#define __CommonTools_h__
+
+#define Z_pdg_mass 91187.6
+#define mu_pdg_mass 105.65836
+#define el_pdg_mass 0.510998910
+#define Z_pdg_width 2495.2
+
+#include "egammaAnalysisUtils/EnergyRescaler.h"
+
+#include "HiggsAnalysis/HSG2EventReader.h"
+#include "HiggsllqqAnalysis/ChargedLepton.h"
+#include <TLorentzVector.h>
+#include "CLHEP/Matrix/Matrix.h"
+#include <iostream>
+
+class Matrix;
+
+namespace Muon {
+   enum {
+      STACO,
+      MUID,
+      CALO,
+   };
+};
+
+namespace Electron {
+   enum {
+      GSF,
+      noGSF,
+   };
+};
+
+namespace CommonTools {
+   TLorentzVector getVector(D3PDReader::TruthParticleD3PDObjectElement *p);
+   Float_t getBremFitDp(D3PDReader::ElectronD3PDObjectElement *el);
+   Float_t getEnergyUncertainty(eg2011::EnergyRescaler *rescaler, Analysis::ChargedLepton *lep);
+   HepMatrix getCovarianceMatrixMuon(Analysis::ChargedLepton *lep);
+   HepMatrix getCovarianceMatrixElectron(eg2011::EnergyRescaler *rescaler, Analysis::ChargedLepton *lep);
+   HepMatrix getCovarianceMatrix(eg2011::EnergyRescaler *rescaler, Analysis::ChargedLepton *lep);
+};
+
+#endif
