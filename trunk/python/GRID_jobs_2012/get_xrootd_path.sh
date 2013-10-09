1d81208cc1a4195489383d304b2c57dd4df3a69c
Arturo Rodolfo Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2013-10-09 11:50:35 +0000

    ...more GRID + PoD Technology

--- trunk/python/GRID_jobs_2012/get_xrootd_path.sh
+++ trunk/python/GRID_jobs_2012/get_xrootd_path.sh
@@ -11,7 +11,7 @@ PRODUCTION=$1                                                                   
 ##                                                                                                                                                                    ##
 rm                             xrootd_paths_$PRODUCTION.txt   path_xrootd_PoD_$PRODUCTION.txt                                                                         ##
 ./xrootd_HZZllqq_$PRODUCTION > xrootd_paths_$PRODUCTION.txt                                                                                                           ##
-grep *.output.root             xrootd_paths_$PRODUCTION.txt > path_xrootd_PoD_$PRODUCTION.txt                                                                         ##
+grep  .output.root             xrootd_paths_$PRODUCTION.txt > path_xrootd_PoD_$PRODUCTION.txt                                                                         ##
 echo "       ------ The xrootd path for the production $PRODUCTION have been found and are saved them into the file path_xrootd_PoD_$PRODUCTION.txt ------        ";  ##
 ##                                                                                                                                                                    ##
 ########################################################################################################################################################################
