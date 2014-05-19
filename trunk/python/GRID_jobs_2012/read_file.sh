6de8b40e1e897f550d757a01b8a3ca1679242f97
Arturo Rodolfo Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2014-05-19 16:56:05 +0000

    New Scripts to launch jobs, stating since version 22.0

--- trunk/python/GRID_jobs_2012/read_file.sh
+++ trunk/python/GRID_jobs_2012/read_file.sh
@@ -8,19 +8,22 @@ SYST=$6
 PRODUCTION=$3.$6
 FilesperJob="1" ## Reempacing the option: --nGBPerJob=$GbperJob
 SitesToSave="INFN-ROMA1_LOCALGROUPDISK,INFN-NAPOLI-ATLAS_LOCALGROUPDISK"
+CMT="--cmtConfig x86_64-slc6-gcc47-opt"
+ROOTVer="--rootVer 5.34.14"
+
 
 if [ $4 = "yes" ]
 then
    echo "        ## Tar File Creation ON... in progress now!! ##  "
    echo "  "
-   echo "prun --exec \"echo %IN > input.txt; python HiggsllqqAnalysis/python/regolari.py; cat input.txt; ./HiggsllqqAnalysis/bin/test --analysis rel_17_2 --useTopoIso --input input.txt --output output.root --MV1c --GSC\" --outDS user.$2.169039.ggH580CPS_ZZllqq.NTUP_HSG2_XXX --outputs output.root --useAthenaPackages --inDS mc12_8TeV.169039.PowhegPythia8_AU2CT10_ggH580CPS_ZZllqq.merge.NTUP_HSG2.e1622_s1581_s1586_r3658_r3549_p1344/ --nFilesPerJob=$FilesperJob --useRootCore --extFile=*/*/*.root*,*/*/*/*.root*,*/*/*/*/*.root*,*/*/*/*/*/*.root*, --outTarBall tarball.tar --noSubmit --destSE=$SitesToSave "
+   echo "prun $CMT --exec \"echo %IN > input.txt; python HiggsllqqAnalysis/python/regolari.py; cat input.txt; ./HiggsllqqAnalysis/bin/test --analysis rel_17_2 --useTopoIso --input input.txt --output output.root --MV1c --GSC\" --outDS user.$2.169039.ggH580CPS_ZZllqq.NTUP_HSG2_XXX --outputs output.root --inDS mc12_8TeV.169039.PowhegPythia8_AU2CT10_ggH580CPS_ZZllqq.merge.NTUP_HSG2.e1622_s1581_s1586_r3658_r3549_p1649/ --nFilesPerJob=$FilesperJob --useRootCore --extFile=*/*/*.root*,*/*/*/*.root*,*/*/*/*/*.root*,*/*/*/*/*/*.root*, --outTarBall tarball.tar --noSubmit --noCompile $ROOTVer --destSE=$SitesToSave "
 fi
 
 if [ $4 = "not" ]
 then
    echo "        ## Tar File Creation OFF, if you need the tar, please, take it from the \"Bank of Tar\" or uncomment the line bellow ##  "
    echo "  "
-   echo "## prun --exec \"echo %IN > input.txt; python HiggsllqqAnalysis/python/regolari.py; cat input.txt; ./HiggsllqqAnalysis/bin/test --analysis rel_17_2 --useTopoIso --input input.txt --output output.root --MV1c --GSC\" --outDS user.$2.169039.ggH580CPS_ZZllqq.NTUP_HSG2_XXXX --outputs output.root --useAthenaPackages --inDS mc12_8TeV.169039.PowhegPythia8_AU2CT10_ggH580CPS_ZZllqq.merge.NTUP_HSG2.e1622_s1581_s1586_r3658_r3549_p1344/ --nFilesPerJob=$FilesperJob --useRootCore --extFile=*/*/*.root*,*/*/*/*.root*,*/*/*/*/*.root*,*/*/*/*/*/*.root*, --outTarBall tarball.tar --noSubmit --destSE=$SitesToSave "
+   echo "## prun $CMT --exec \"echo %IN > input.txt; python HiggsllqqAnalysis/python/regolari.py; cat input.txt; ./HiggsllqqAnalysis/bin/test --analysis rel_17_2 --useTopoIso --input input.txt --output output.root --MV1c --GSC\" --outDS user.$2.169039.ggH580CPS_ZZllqq.NTUP_HSG2_XXX --outputs output.root --inDS mc12_8TeV.169039.PowhegPythia8_AU2CT10_ggH580CPS_ZZllqq.merge.NTUP_HSG2.e1622_s1581_s1586_r3658_r3549_p1649/ --nFilesPerJob=$FilesperJob --useRootCore --extFile=*/*/*.root*,*/*/*/*.root*,*/*/*/*/*.root*,*/*/*/*/*/*.root*, --outTarBall tarball.tar --noSubmit --noCompile $ROOTVer --destSE=$SitesToSave "
 fi
 
 echo "        ## ";date; echo "  "
@@ -36,9 +39,9 @@ while read files;do
         sample=${sample//.merge.NTUP_HSG2/}
         if [ $5 = "" ] ##        if [ $5 = "" ]
         then
-           echo "prun --exec \"echo %IN > input.txt; python HiggsllqqAnalysis/python/regolari.py; cat input.txt; ./HiggsllqqAnalysis/bin/test --analysis rel_17_2 --useTopoIso --input input.txt --output output.root --MV1c --GSC --DoSystematic $SYST\" --outDS user.$2.$sample.$PRODUCTION --outputs output.root --useAthenaPackages --inDS $line --nFilesPerJob=$FilesperJob --useRootCore --extFile=*/*/*.root*,*/*/*/*.root*,*/*/*/*/*.root*,*/*/*/*/*/*.root*, --inTarBall tarball.tar --destSE=$SitesToSave --useShortLivedReplicas"  
+           echo "prun $CMT --exec \"echo %IN > input.txt; python HiggsllqqAnalysis/python/regolari.py; cat input.txt; ./HiggsllqqAnalysis/bin/test --analysis rel_17_2 --useTopoIso --input input.txt --output output.root --MV1c --GSC\" --outDS user.$2.$sample.$PRODUCTION --outputs output.root --inDS $line --nFilesPerJob=$FilesperJob --useRootCore --extFile=*/*/*.root*,*/*/*/*.root*,*/*/*/*/*.root*,*/*/*/*/*/*.root*, --inTarBall tarball.tar --noCompile $ROOTVer --destSE=$SitesToSave --useShortLivedReplicas"
         else
-           echo "prun --exec \"echo %IN > input.txt; python HiggsllqqAnalysis/python/regolari.py; cat input.txt; ./HiggsllqqAnalysis/bin/test --analysis rel_17_2 --useTopoIso --input input.txt --output output.root --MV1c --GSC --DoSystematic $SYST\" --outDS user.$2.$sample.$PRODUCTION --outputs output.root --useAthenaPackages --inDS $line --nFilesPerJob=$FilesperJob --useRootCore --extFile=*/*/*.root*,*/*/*/*.root*,*/*/*/*/*.root*,*/*/*/*/*/*.root*, --inTarBall tarball.tar --destSE=$SitesToSave --useShortLivedReplicas --excludedSite=$5"   
+           echo "prun $CMT --exec \"echo %IN > input.txt; python HiggsllqqAnalysis/python/regolari.py; cat input.txt; ./HiggsllqqAnalysis/bin/test --analysis rel_17_2 --useTopoIso --input input.txt --output output.root --MV1c --GSC\" --outDS user.$2.$sample.$PRODUCTION --outputs output.root --inDS $line --nFilesPerJob=$FilesperJob --useRootCore --extFile=*/*/*.root*,*/*/*/*.root*,*/*/*/*/*.root*,*/*/*/*/*/*.root*, --inTarBall tarball.tar --noCompile $ROOTVer --destSE=$SitesToSave --useShortLivedReplicas --excludedSite=$5"
         fi
         ((k++))
     done < $files
