73c79fa69fb26021f63765345faa27228d6d8fc4
Arturo Rodolfo Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2014-05-20 18:22:31 +0000

    script to find the dpm path for proof and other apps

--- trunk/python/GRID_jobs_2012/example_of_how_to_get_xrootd_path.sh
+++ trunk/python/GRID_jobs_2012/example_of_how_to_get_xrootd_path.sh
@@ -1,6 +1,3 @@
-## October 7th 2013
-dq2-ls -L INFN-ROMA1_LOCALGROUPDISK -fp  user.arturos.mc12_8TeV.117293.AlpgenJimmy_AUET2CTEQ6L1_WcNp0.merge.NTUP_HSG2.e1601_s1499_s1504_r3658_r3549_p1344.13.1/|grep srm|awk '{print "lcg-la "$1}'|awk '{system($0 "|sed 's#lfn:/grid#root://t2-dpm-01.na.infn.it:1094/#g'")}'
-
-## November 28th 2013
- dq2-ls -L INFN-NAPOLI-ATLAS_PHYS-HIGGS -fp  mc12_8TeV.105200.McAtNloJimmy_CT10_ttbar_LeptonFilter.merge.NTUP_HSG2.e1513_s1499_s1504_r3658_r3549_p1344/|grep srm|awk '{print "lcg-la "$1}'|awk '{system($0 "|sed 's#lfn:/grid#root://t2-dpm-01.na.infn.it:1094/#g'")}'    
-
+dq2-ls -L INFN-ROMA1_LOCALGROUPDISK -fp  user.arturos.mc12_8TeV.117293.AlpgenJimmy_AUET2CTEQ6L1_WcNp0.merge.NTUP_HSG2.e1601_s1499_s1504_r3658_r3549_p1344.13.1/|grep srm |grep \.root >_tmp_ 
+sed 's#dpm#/dpm#g' _tmp_ >__tmp_; mv __tmp_ _tmp_
+sed 's#srm://#root://#g' _tmp_ >__tmp_; mv __tmp_ _tmp_
